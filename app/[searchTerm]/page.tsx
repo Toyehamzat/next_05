7be25b9fc4ace221e4a25314314db502f7aebbd6
Toyehamzat <anibiseun@gmail.com>
2023-08-17 20:16:25 +0100

    JSON returned

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,9 +1,23 @@
+import GetWikiResult from "@/lib/getWikiResult";
 type Props = {
-    params:{
-        searchTerm:string
-    }
+  params: {
+    searchTerm: string;
+  };
 };
 
-export default function page({params:{searchTerm}}: Props) {
-  return <div>page</div>;
+export default async function page({ params: { searchTerm } }: Props) {
+  const Wikidata: Promise<SearchResult> = GetWikiResult(searchTerm);
+  const data = await Wikidata;
+  const results: Result[] | undefined = data?.query?.pages;
+  return (
+    <main className=" max-w-lg py-1  min-h-screen">
+      {results ? (
+        Object.values(results).map((result) => {
+          return <p>{JSON.stringify(result)}</p>;
+        })
+      ) : (
+        <h1 className=" max-w-lg py-1  min-h-screen">{`${searchTerm} Not found`}</h1>
+      )}
+    </main>
+  );
 }
